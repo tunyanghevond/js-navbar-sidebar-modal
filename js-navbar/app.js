f0437444954d698137d4074e27734bc4281aa258
Ghevond Tunyan <ghtunyan@gmail.com>
2022-05-09 16:42:06 +0200

    add navbar project

---
+++ js-navbar/app.js
@@ -0,0 +1,19 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const toggleBtn = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+toggleBtn.addEventListener('click', function () {
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add('show-links')
+    // }
+    links.classList.toggle('show-links');
+    console.log(links.classList)
+})
+
